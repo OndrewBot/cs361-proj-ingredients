bc27bcc196b0a87d30978dc3b84e29a66f210e21
Andrew Bottom <bottoma@oregonstate.edu>
2024-08-10 14:53:24 -0400

    formatting

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['https://recipe-builder.onrender.com/'],
+    allow_origins=['https://recipe-builder.onrender.com/*'],
     allow_methods=['*'],
     allow_headers=['*']
 )
